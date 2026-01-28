3c35955a813415af712fc7453358222380bbf486
adamelouali15-ai <adamelouali15@gmail.com>
2026-01-28 16:02:17 +0100

    miau

---
+++ taschenrechner.py
@@ -0,0 +1,9 @@
+def taschenrechner():
+    zahl1 = float(input("Geben Sie die erste Zahl ein: "))
+    zahl2 = float(input("Geben Sie die zweite Zahl ein: "))
+    summe = zahl1 + zahl2
+    print(f"Das Ergebnis der Addition lautet: {summe}")
+
+if __name__ == "__main__":
+    taschenrechner()
+
