d814aa3570a51777da285c093f2ffbfd743c4c05
adamelouali15-ai <adamelouali15@gmail.com>
2026-01-29 10:12:58 +0100

    Main taschenrechner

--- taschenrechner.py
+++ taschenrechner.py
@@ -1,9 +1,27 @@
+import rechenoperationen as ro # type: ignore
+
 def taschenrechner():
     zahl1 = float(input("Geben Sie die erste Zahl ein: "))
     zahl2 = float(input("Geben Sie die zweite Zahl ein: "))
-    summe = zahl1 + zahl2
-    print(f"Das Ergebnis der Addition lautet: {summe}")
 
+    operations = ["add", "sub", "mult", "div"]
+    print(f"Diese Operationen stehen zur Verfügung: {operations}")
+    selection = input("Geben Sie eine Operation an: ")
+
+    if selection == "add":
+        result = ro.addition(zahl1, zahl2)
+    elif selection == "sub":
+        result = ro.subtraction(zahl1, zahl2)
+    elif selection == "mult":
+        result = ro.multiplication(zahl1, zahl2)
+    elif selection == "div":
+        result = ro.division(zahl1, zahl2)
+    else:
+        print("Ungültige Auswahl")
+        return
+
+    print(f"Das Ergebnis lautet: {result}")
 if __name__ == "__main__":
     taschenrechner()
+# Dies ist ein einfacher Taschenrechner, der Addition, Subtraktion, Multiplikation und Division unterstützt.
 
